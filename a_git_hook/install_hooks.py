8fae36c22db4356e655ea9a2570e207a1eb20c5d
horatio <horatio@>
2023-12-03 00:55:18 -0600

    [:master:] refactoring

--- a_git_hook/install_hooks.py
+++ a_git_hook/install_hooks.py
@@ -4,7 +4,7 @@ hook_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".git", "hoo
 
 orig_hook = "post-commit"
 
-hook_files = list(map(str.strip, """
+hook_text_list = """
 post-commit
 post-checkout
 post-receive
@@ -12,14 +12,17 @@ post-merge
 post-update
 push-to-checkout
 push-to-deploy
-""".split("\n")[1:-1]))
+"""
 
+if __name__ == '__main__':
 
-file_content = r"""
+    hook_files = list(map(str.strip, hook_text_list.split("\n")[1:-1]))
+
+    file_content = r"""
 #!/bin/sh
 python -m a_git_hook.update_version
-""".lstrip()
+    """.lstrip()
 
-for file in hook_files:
-    with open(os.path.join(hook_dir, file), "w") as fp:
-        fp.write(file_content)
+    for file in hook_files:
+        with open(os.path.join(hook_dir, file), "w") as fp:
+            fp.write(file_content)
